1cf6259ec81ce9f3de366ef81a7a5c0716adbd64
longjxchina <longjxchina@qq.com>
2014-06-04 16:22:32 +0800

    新增 自定义二维码内容

--- Src/js/popup.js
+++ Src/js/popup.js
@@ -10,4 +10,33 @@
             text: tablink
         });    
     }); // end of chrome.tabs.getSelected(null,function(tab) {
+
+    $("#btnGenerate").click(function(){
+        var $content = $("#txtCstmContent");
+        var contentVal = $.trim($content.val());
+
+        if (contentVal.length == 0){
+            $content.focus();
+            $content.css("border-color", "red");
+            return;
+        }
+
+        $content.css("border-color", "green");
+
+        clearCanvas("qr");
+        $("#qr").qrcode({
+            width: 200,
+            height: 200,
+            color: '#3a3',
+            text: contentVal
+        });
+
+        $(".modal").modal("hide");
+    }); // end of $("#btnGenerate").click(function(){
+
+    window.clearCanvas = function(id){
+        var qrCanvas = document.getElementById(id).firstChild;
+        
+        qrCanvas.remove();
+    }
 })(jQuery); // end of !(function($){
