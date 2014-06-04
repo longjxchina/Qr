ef9d9d4da6e3431b6840c617e61a131e1430067f
longjxchina <longjxchina@qq.com>
2014-06-04 18:43:34 +0800

    修改 自定义支持中文；

--- Src/js/popup.js
+++ Src/js/popup.js
@@ -4,11 +4,14 @@
         var tablink = tab.url;        
 
         $("#qr").qrcode({
-            width: 200,
-            height: 200,
-            color: '#3a3',
-            text: tablink
-        });    
+            width: width,
+            height: width,
+            color: '#3a3',            
+            text: utf16to8(tablink)
+        });  
+
+        $("#qr canvas").width(width);
+        $("#qr canvas").height(width);
     }); // end of chrome.tabs.getSelected(null,function(tab) {
 
     $("#btnGenerate").click(function(){
@@ -25,18 +28,42 @@
 
         clearCanvas("qr");
         $("#qr").qrcode({
-            width: 200,
-            height: 200,
+            width: width,
+            height: width,
             color: '#3a3',
-            text: contentVal
+            text: utf16to8(contentVal)
         });
 
+        $("#qr canvas").width(width);
+        $("#qr canvas").height(width);
         $(".modal").modal("hide");
     }); // end of $("#btnGenerate").click(function(){
+
+    window.width = 200;
 
     window.clearCanvas = function(id){
         var qrCanvas = document.getElementById(id).firstChild;
         
         qrCanvas.remove();
     }
+
+    window.utf16to8 = function (str) {  
+            var out, i, len, c;  
+            out = "";  
+            len = str.length;  
+            for (i = 0; i < len; i++) {  
+                c = str.charCodeAt(i);  
+                if ((c >= 0x0001) && (c <= 0x007F)) {  
+                    out += str.charAt(i);  
+                } else if (c > 0x07FF) {  
+                    out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));  
+                    out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));  
+                    out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));  
+                } else {  
+                    out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));  
+                    out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));  
+                }  
+            }  
+            return out;  
+        }  
 })(jQuery); // end of !(function($){
